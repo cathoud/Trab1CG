6e4388b78f23e87dcef4dd3afcffe2935eab8bf3
cathoud <cathoud@hotmail.com>
2015-10-03 00:08:25 -0300

    Genius Completo

--- Mine2.cpp
+++ Mine2.cpp
@@ -42,8 +42,8 @@ float r9, g9, b9;
 bool clicou = false;
 bool animation = true;
 bool aceso = true;  
-void gerencia_mouse_3x3(int button, int state, int x, int y);
-void gerencia_mouse_2x2(int button, int state, int x, int y);
+bool modo2x2 = true;
+void gerencia_mouse(int button, int state, int x, int y);
 void acende_quadrado_sorteado_3x3();
 void acende_quadrado_sorteado_2x2();
 void mostra_sequencia_sorteados_3x3();
@@ -78,22 +78,29 @@ void reseta() {
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Comeca memoria dois por dois
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void logica2(int);
-void desenha_quad();
+void DISPLAY_SEQUENCIA(int);
+void desenha_quad_sequencia();
 void desenha_quad_usuario();
-void sorteiaQuadrados();
+void sorteiaQuadrados(int);
+void desenha_quadrados_3x3(int);
+void desenha_quadrados_2x2(int);
 void PRE_MOSTRAR_SEQUENCIA(int);
-void renderiza_quadrados_apagados_2x2();
+
 void dois_por_dois() {
     reseta();
+    modo2x2 = true;
     sorteiaQuadrados(4);
-    renderiza_quadrados_apagados_2x2();
+    desenha_quadrados_2x2(-1);
     glutTimerFunc(WAITTIME,PRE_MOSTRAR_SEQUENCIA,0);
 }
 
 void tres_por_tres() {
     reseta();
+    modo2x2 = false;
     sorteiaQuadrados(9);
+    desenha_quadrados_3x3(-1);
+    glutTimerFunc(WAITTIME,PRE_MOSTRAR_SEQUENCIA,0);
+
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Tela branca
@@ -138,7 +145,8 @@ void tela_go() {
 //Renderiza quadrados apagados
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void renderiza_quadrados_apagados_2x2() {
+void desenha_quadrados_2x2(int quad) {
+
     glBegin(GL_QUADS); //primeiro
     glColor3f(0.7, 0.0, 0.0);
     glVertex2f(-win, 0); // x, y
@@ -170,15 +178,62 @@ void renderiza_quadrados_apagados_2x2() {
     glVertex2f(win, 0);
     glVertex2f(win, -win);
     glEnd();
-    glFlush();
 
+    //Paleta de cores
+    r1 = 1.0, g1 = 0.0, b1 = 0.0;
+    r2 = 0.0, g2 = 1.0, b2 = 0.0;
+    r3 = 0.0, g3 = 0.0, b3 = 1.0;
+    r4 = 1.0, g4 = 1.0, b4 = 0.0;
+
+    switch (quad) {
+        case -1:
+            break;
+        case 0:
+            glBegin(GL_QUADS); //primeiro
+            glColor3f(r1, g1, b1);
+            glVertex2f(-win, 0); // x, y
+            glVertex2f(-win, win);
+            glVertex2f(0, win);
+            glVertex2f(0, 0);
+            glEnd();
+            break;
+        case 1:  
+            glBegin(GL_QUADS); //segundo
+            glColor3f(r2, g2, b2);
+            glVertex2f(0, 0); // x, y
+            glVertex2f(0, win);
+            glVertex2f(win, win);
+            glVertex2f(win, 0);
+            glEnd();
+            break;
+        case 2:
+            glBegin(GL_QUADS); //terceiro
+            glColor3f(r3, g3, b3);
+            glVertex2f(-win, -win); // x, y
+            glVertex2f(-win, 0);
+            glVertex2f(0, 0);
+            glVertex2f(0, -win);
+            glEnd();
+            break;
+        case 3:
+            glBegin(GL_QUADS); //quarto
+            glColor3f(r4, g4, b4);
+            glVertex2f(0, -win); // x, y
+            glVertex2f(0, 0);
+            glVertex2f(win, 0);
+            glVertex2f(win, -win);
+            glEnd();
+            break;
+    }
+
+    glFlush();
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Verifica quadrado clicado na tela e depois confere se este esta de acordo com a sequencia ja sorteada
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void gerencia_mouse_2x2(int button, int state, int x, int y) {
+void gerencia_mouse(int button, int state, int x, int y) {
     if (button == GLUT_LEFT_BUTTON) {
         if(clicou) {
             if(state == GLUT_UP) {
@@ -202,7 +257,11 @@ void gerencia_mouse_2x2(int button, int state, int x, int y) {
             aceso = true;
 
             //Obtem valores em relação a resolução da tela
-            clickquad = 2*(y/(win/2))+x/(win/2);
+            if(modo2x2) {
+                clickquad = 2*(y/(win/2))+x/(win/2);
+            } else {
+                clickquad = 3*(y/(win/3))+x/(win/3);    
+            }
             user = clickquad;           
 
         } else if(state == GLUT_UP) {
@@ -212,159 +271,41 @@ void gerencia_mouse_2x2(int button, int state, int x, int y) {
     glutPostRedisplay();
 }
 
-void desenha_quadrados(int quad) {
-    //Paleta de cores
-    r1 = 1.0, g1 = 0.0, b1 = 0.0;
-    r2 = 0.0, g2 = 1.0, b2 = 0.0;
-    r3 = 0.0, g3 = 0.0, b3 = 1.0;
-    r4 = 1.0, g4 = 1.0, b4 = 0.0;    
+/*
+void gerencia_mouse(int button, int state, int x, int y) {
+    if (button == GLUT_LEFT_BUTTON) {
+        if(clicou) {
+            if(state == GLUT_UP) {
+                if(valores_sorteados[poslida] != user) {
+                    glutDisplayFunc(tela_gameover);
+                    glutMouseFunc(NULL);
+                } else if(poslida == SIZE-1) {
+                    glutDisplayFunc(tela_venceu);
+                } else if(poslida < posMaxLida) {
+                    poslida++;
+                } else {
+                    posMaxLida++;
+                    glutTimerFunc(100,PRE_MOSTRAR_SEQUENCIA,0);
+                }
+                clicou = false;
+            }
+        } 
 
-    switch (quad) {
-        case 0:
-            glBegin(GL_QUADS); //primeiro
-            glColor3f(r1, g1, b1);
-            glVertex2f(-win, 0); // x, y
-            glVertex2f(-win, win);
-            glVertex2f(0, win);
-            glVertex2f(0, 0);
-            glEnd();
+        if (state == GLUT_DOWN) {
+            clicou = true;
+            aceso = true;
 
-            glBegin(GL_QUADS); //quarto
-            glColor3f(0.7, 0.7, 0.0);
-            glVertex2f(0, -win); // x, y
-            glVertex2f(0, 0);
-            glVertex2f(win, 0);
-            glVertex2f(win, -win);
-            glEnd();
+            //Obtem valores em relação a resolução da tela
+            clickquad = 3*(y/(win/3))+x/(win/3);
+            user = clickquad;           
 
-            glBegin(GL_QUADS); //terceiro
-            glColor3f(0.0, 0.0, 0.7);
-            glVertex2f(-win, -win); // x, y
-            glVertex2f(-win, 0);
-            glVertex2f(0, 0);
-            glVertex2f(0, -win);
-            glEnd();
-
-            glBegin(GL_QUADS); //segundo
-            glColor3f(0.0, 0.7, 0.0);
-            glVertex2f(0, 0); // x, y
-            glVertex2f(0, win);
-            glVertex2f(win, win);
-            glVertex2f(win, 0);
-            glEnd();
-
-            glFlush();
-            break;
-
-        case 1:
-            glBegin(GL_QUADS); //segundo
-            glColor3f(r2, g2, b2);
-            glVertex2f(0, 0); // x, y
-            glVertex2f(0, win);
-            glVertex2f(win, win);
-            glVertex2f(win, 0);
-            glEnd();
-
-            glBegin(GL_QUADS); //quarto
-            glColor3f(0.7, 0.7, 0.0);
-            glVertex2f(0, -win); // x, y
-            glVertex2f(0, 0);
-            glVertex2f(win, 0);
-            glVertex2f(win, -win);
-            glEnd();
-
-            glBegin(GL_QUADS); //terceiro
-            glColor3f(0.0, 0.0, 0.7);
-            glVertex2f(-win, -win); // x, y
-            glVertex2f(-win, 0);
-            glVertex2f(0, 0);
-            glVertex2f(0, -win);
-            glEnd();
-
-            glBegin(GL_QUADS); //primeiro
-            glColor3f(0.7, 0.0, 0.0);
-            glVertex2f(-win, 0); // x, y
-            glVertex2f(-win, win);
-            glVertex2f(0, win);
-            glVertex2f(0, 0);
-            glEnd();
-
-            glFlush();
-            break;
-
-        case 2:
-            glBegin(GL_QUADS); //terceiro
-            glColor3f(r3, g3, b3);
-            glVertex2f(-win, -win); // x, y
-            glVertex2f(-win, 0);
-            glVertex2f(0, 0);
-            glVertex2f(0, -win);
-            glEnd();
-
-            glBegin(GL_QUADS); //quarto
-            glColor3f(0.7, 0.7, 0.0);
-            glVertex2f(0, -win); // x, y
-            glVertex2f(0, 0);
-            glVertex2f(win, 0);
-            glVertex2f(win, -win);
-            glEnd();
-
-            glBegin(GL_QUADS); //segundo
-            glColor3f(0.0, 0.7, 0.0);
-            glVertex2f(0, 0); // x, y
-            glVertex2f(0, win);
-            glVertex2f(win, win);
-            glVertex2f(win, 0);
-            glEnd();
-
-            glBegin(GL_QUADS); //primeiro
-            glColor3f(0.7, 0.0, 0.0);
-            glVertex2f(-win, 0); // x, y
-            glVertex2f(-win, win);
-            glVertex2f(0, win);
-            glVertex2f(0, 0);
-            glEnd();
-
-            glFlush();
-            break;
-
-        case 3:
-            glBegin(GL_QUADS); //quarto
-            glColor3f(r4, g4, b4);
-            glVertex2f(0, -win); // x, y
-            glVertex2f(0, 0);
-            glVertex2f(win, 0);
-            glVertex2f(win, -win);
-            glEnd();
-
-            glBegin(GL_QUADS); //terceiro
-            glColor3f(0.0, 0.0, 0.7);
-            glVertex2f(-win, -win); // x, y
-            glVertex2f(-win, 0);
-            glVertex2f(0, 0);
-            glVertex2f(0, -win);
-            glEnd();
-
-            glBegin(GL_QUADS); //segundo
-            glColor3f(0.0, 0.7, 0.0);
-            glVertex2f(0, 0); // x, y
-            glVertex2f(0, win);
-            glVertex2f(win, win);
-            glVertex2f(win, 0);
-            glEnd();
-
-            glBegin(GL_QUADS); //primeiro
-            glColor3f(0.7, 0.0, 0.0);
-            glVertex2f(-win, 0); // x, y
-            glVertex2f(-win, win);
-            glVertex2f(0, win);
-            glVertex2f(0, 0);
-            glEnd();
-
-            glFlush();
-            break;   
+        } else if(state == GLUT_UP) {
+            aceso = false;
+        }
     }
+    glutPostRedisplay();
 }
+*/
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Animação Mostrar Sequencia
@@ -374,31 +315,31 @@ void PRE_MOSTRAR_SEQUENCIA(int val) {
     glutMouseFunc(NULL);
     poslida = 0;
     animation = true;
-    glutDisplayFunc(desenha_quad);
-    glutTimerFunc(WAITTIME,logica2,1);
+    glutDisplayFunc(desenha_quad_sequencia);
+    glutTimerFunc(WAITTIME,DISPLAY_SEQUENCIA,1);
 }
 
 void PLAYING(int val) {
     glutDisplayFunc(desenha_quad_usuario);
     glutPostRedisplay();
     poslida = 0;
-    glutMouseFunc(gerencia_mouse_2x2);
+    glutMouseFunc(gerencia_mouse);
 }
 
 void GO(int val) {
     if(val) {
         aceso = false;
-        glutTimerFunc(WAITTIME,GO,0);
+        glutTimerFunc(WAITTIME/2,GO,0);
     } else {
         glutDisplayFunc(tela_go);
-        glutTimerFunc(1.5*WAITTIME,PLAYING,0);
+        glutTimerFunc(WAITTIME,PLAYING,0);
     }
     glutPostRedisplay();
 }
 
-void logica2(int val) {
+void DISPLAY_SEQUENCIA(int val) {
     if(animation) {
-        glutTimerFunc(WAITTIME,logica2,!val);
+        glutTimerFunc(WAITTIME,DISPLAY_SEQUENCIA,!val);
         if(val) {
             aceso = true;
         } else {
@@ -413,18 +354,32 @@ void logica2(int val) {
     }
 }
 
-void desenha_quad() {
-    if(aceso)
-        desenha_quadrados(valores_sorteados[poslida]);
-    else
-        renderiza_quadrados_apagados_2x2();
+void desenha_quad_sequencia() {
+    if(aceso) {
+        if(modo2x2)
+            desenha_quadrados_2x2(valores_sorteados[poslida]);
+        else
+            desenha_quadrados_3x3(valores_sorteados[poslida]);
+    } else {
+        if(modo2x2)
+            desenha_quadrados_2x2(-1);
+        else
+            desenha_quadrados_3x3(-1);
+    }
 }
 
 void desenha_quad_usuario() {
-    if(aceso)
-        desenha_quadrados(user);
-    else
-        renderiza_quadrados_apagados_2x2();
+    if(aceso) {
+        if(modo2x2)
+            desenha_quadrados_2x2(user);
+        else
+            desenha_quadrados_3x3(user);
+    } else {
+        if(modo2x2)
+            desenha_quadrados_2x2(-1);
+        else
+            desenha_quadrados_3x3(-1);
+    }
 }
 
 void sorteiaQuadrados(int val) {
@@ -540,24 +495,10 @@ void redimensionamento(GLsizei w, GLsizei h) {
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Verifica quadrado clicado na tela e depois confere se este esta de acordo com a sequencia ja sorteada
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-void gerencia_mouse_3x3(int button, int state, int x, int y) {
-    if (button == GLUT_LEFT_BUTTON) {
-        if (state == GLUT_DOWN) {
-
-            //Verifica em qual parte da tela se encontra determinado quadrado
-        }
-    }
-    glutPostRedisplay();
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Vai acendendo os quadrados ja sorteados na sequencia salva no array
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void quadrado_aceso_3x3(int quad) {
+void desenha_quadrados_3x3(int quad) {
     renderiza_quadrados_apagados_3x3();
 
     //Paleta de cores
@@ -572,7 +513,7 @@ void quadrado_aceso_3x3(int quad) {
     r9 = 0.5, g9 = 0.7, b9 = 1.0;
 
     switch (quad) {
-        case 9:
+        case -1:
             break;
         case 0:
             glBegin(GL_QUADS); //primeiro
